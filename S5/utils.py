641ee4955566b24c8154b16eeaac6cfdacc2d6b7
ksharsha72 <Harsha070@outlook.com>
2024-02-24 03:50:11 +0530

    Modular S5 Code

---
+++ S5/utils.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from model import Net
+
+
+def is_cuda_enabled():
+    cuda = torch.cuda.is_available()
+    print("CUDA Available?", cuda)
+    return cuda
+
+
+#Train Transforms
+train_transforms = transforms.Compose([
+    transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
+    transforms.Resize((28, 28)),
+    transforms.RandomRotation((-15., 15.), fill=0),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,)),
+    ])
+
+# Test data transformations
+test_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1407,), (0.4081,))
+    ])
+
+
+
+train_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
+test_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
+
+
+batch_size = 512
+
+kwargs = {'batch_size': batch_size, 'shuffle': False, 'num_workers': 2, 'pin_memory': True}
+
+test_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+
+# Data to plot accuracy and loss graphs
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+
+device = torch.device("cuda" if is_cuda_enabled() else "cpu")
+model = Net().to(device)
+
+
+
